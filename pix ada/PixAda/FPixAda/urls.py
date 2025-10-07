d2b6d00120be0f85ebbf9724905195de9db5c49f
Hatchlord <altamiranoaxius@gmail.com>
2025-10-07 12:41:45 -0300

    Renderizado de páginas creado
    Con documentación para ser replicable por equipo Django y probablemente back-end

--- pix ada/PixAda/FPixAda/urls.py
+++ pix ada/PixAda/FPixAda/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 
 from . import views
 
+'''
+De Axius:
+Las 'views' servirán para crear las páginas de la app web, en este caso, en la app FPixAda podemos específicar
+cómo se verán las URLs con el siguiente formato:
+    path("<subURL>, views.<funciónVIEW>, name="<nombreURL>")
+
+La URL se debería ver algo así:
+http://127.0.0.1:<puerto>/FPixAda/<subURL>/
+
+Comentaré como se vería la url de cada view abajo para claridad
+Diviértete Isi
+'''
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"), ## http://localhost:<puerto>/FPixAda/          Esta es la página origen de la API
+    path("time/", views.timePage, name="time"), ## http://localhost:<puerto>/FPixAda/time/
 ]
--- pix ada/PixAda/FPixAda/views.py
+++ pix ada/PixAda/FPixAda/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+
+# Crea tus views
 def index(request):
-    return HttpResponse("This is a test")
+    __contexto = {
+        'pestana': 'FPixAda',
+        'tit': 'Prueba título (se llama header, pero no importa mucho)' ## El nombre fué a propósito xdd
+    }
+    return render(request, 'index.html', __contexto)
+
+def timePage(request):
+    return HttpResponse('ass')
+
+'''
+De Axius:
+Con esto vamos a decirle a Django que renderize las páginas web.
+Cada view tiene este formato:
+
+def <view>(request):
+    .
+    .
+    .
+    return <algo>
+
+Ese <algo> puede ser una de dos cosas:
+un HttpResponse que solo muestra un texto en formato String o,
+una función render() que mostrará una página hecha en html que el PABLO debería estar haciendo.
+
+para usar return render() dale 3 parametros
+
+    render(request, <ubicaciónHTML>, <contexto>)
+
+request siempre irá ahí, <ubicaciónHTML> siempre empieza desde la carpeta "templates" de la API,
+si quieres mostrar index.html, solo escribes 'index.html', pero si está en una sub carpeta,
+escribes '<carpeta>/<archHTML>'
+
+el contexto será todo lo interactivo que usará el HTML.
+Digamos que tienes adentro un ciclo que va cambiando cierta variable, crea un diccionario con esa variable que va cambiando,
+la cual será usada en el archivo HTML.
+'''
