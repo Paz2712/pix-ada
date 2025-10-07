d59f05d9d7b41f7dc49bacf14b64fdb384e4d138
Hatchlord <altamiranoaxius@gmail.com>
2025-10-07 12:43:05 -0300

    NO TOCAR
    NO TOCAR X2

--- pix ada/PixAda/PixAda/settings.py
+++ pix ada/PixAda/PixAda/settings.py
@@ -17,10 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -64,7 +60,7 @@ ROOT_URLCONF = 'PixAda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], ## NO TOCAR, estamos usando app-level Templates, osea que cada app tiene sus propias templates para páginas web
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = '-04'
 
 USE_I18N = True
 
